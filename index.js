38bf53c627ef024fc1c46b4c749a6b9266632a4b
egor <piratikcooldog@gmail.com>
2023-03-04 01:02:45 -0800

    1

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express'), bodyParser = require('body-parser');
+const app = express()
+var cors = require('cors')
+const http = require('http').Server(app)
+const socketIo = require('socket.io')(http,{
+    cors: {
+        origin: 'http://localhost:3000'
+    }
+})
+
+socketIo.on('connection', (socket)=>{
+    console.log('user connect');
+    
+    socket.on('message',()=>{
+        console.log('message!');
+    })
+    
+    socket.on('disconect',()=>{
+        console.log('user disconect');
+    })
+})
+app.use(cors())
+app.use(bodyParser.json());
+
+
+app.get('/log', (req, res) => {
+    console.log(file);
+})
+
+http.listen(3010, () => {
+    console.log('server started');
+})
